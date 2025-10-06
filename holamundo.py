1f9753b130cb9a9d7913d3112a93c373e8553042
Tristan Borja <tborzam992@ieszaidinvergeles.org>
2025-10-06 12:36:15 +0200

    ejercicio 1.py

--- holamundo.py
+++ holamundo.py
@@ -1,1 +1,12 @@
+
 print("hola mundo")
+
+
+peso = float(input("Introduce tu peso en kg: "))
+
+estatura = float(input("Introduce tu estatura en metros: "))
+
+imc = peso / (estatura ** 2)
+
+print(f"Tu índice de masa corporal es {imc:.2f}")
+
