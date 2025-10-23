fbfaf31b7d3231561992cca65df5a69fd7f7cddc
Tristan Borja <tborzam992@ieszaidinvergeles.org>
2025-10-23 10:38:00 +0200

    relacion2_9

---
+++ relacion2_9.py
@@ -0,0 +1,8 @@
+dia = input ("introduce tu dia de nacimiento")
+print("tu edad es", dia) 
+mes = input ("introduce tu mes de nacimiento")
+print("tu mes de nacimiento es", mes)
+anonacimiento = input ("introduce tu año de nacimiento")
+print("tu año de nacimiento es", anonacimiento)
+fechatotal = dia + mes + anonacimiento
+print("tu fecha de nacimiento es", fechatotal)
