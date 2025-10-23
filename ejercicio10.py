e4b19e379607f6a5dbb49f41966ac6c3055d5909
Tristan Borja <tborzam992@ieszaidinvergeles.org>
2025-10-23 09:59:51 +0200

    ejercicio10.0

---
+++ ejercicio10.py
@@ -0,0 +1,7 @@
+payasos = float(input("numero de payasos"))
+muneca = float(input("numero de muñecas"))
+
+resuldopayaso = payasos *112
+resuldomuneca = muneca *75
+total = resuldomuneca + resuldopayaso
+print("El peso total es",total)
