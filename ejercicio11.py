47f52cccf7238af0512a6d7a6de19b66b11aa8d1
Tristan Borja <tborzam992@ieszaidinvergeles.org>
2025-10-23 10:06:15 +0200

    ejercicio11

---
+++ ejercicio11.py
@@ -0,0 +1,8 @@
+deposito = float(input("introduce el deposito"))
+interes = 0.04
+ahorro1 = deposito * (1 + interes)
+ahorro2 = ahorro1 * (1 + interes)
+ahorro3 = ahorro2 * (1 + interes)
+print("ahorro1", ahorro1)
+print("ahorro2", ahorro2)
+print("ahorro3", ahorro3)
