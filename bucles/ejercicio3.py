7786c032cfddd034a0f144a75ef049e405e04670
Tristan Borja <tborzam992@ieszaidinvergeles.org>
2025-11-06 10:18:09 +0100

    bucle

---
+++ bucles/ejercicio3.py
@@ -0,0 +1,8 @@
+#Ejercicio 3
+#Escribir un programa que pida al usuario un número entero positivo y muestre por
+#pantalla todos los números impares desde 1 hasta ese número separados por
+#comas.
+
+p=int(input("introduce un numero positivo"))
+for i in range (1,p+1,2):
+    print(i, end=",")
