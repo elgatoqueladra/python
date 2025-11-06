b73ec241a66256bf13c841368114f5a2f97c286f
Tristan Borja <tborzam992@ieszaidinvergeles.org>
2025-11-06 09:28:20 +0100

    relacion

---
+++ Ejercicios de cadenas/relacion2_10.py
@@ -0,0 +1,5 @@
+
+productos = input("Introduce los productos de la cesta de la compra separados por comas: ")
+lista_productos = productos.split (",")
+for producto in lista_productos:
+    print(producto)
